9bbbaf38f99eca3e43a493ddb524bc6c448b8cfa
Jann Horn <jannh@google.com>
2023-07-04 14:49:04 +0000

    mm/slub: sanity-check freepointers
    Sanity-check that:
     - non-NULL freepointers point into the slab
     - freepointers look plausibly aligned
    
    Signed-off-by: Jann Horn <jannh@google.com>
    Co-developed-by: Matteo Rizzo <matteorizzo@google.com>
    Signed-off-by: Matteo Rizzo <matteorizzo@google.com>

--- lib/slub_kunit.c
+++ lib/slub_kunit.c
@@ -45,6 +45,10 @@ static void test_clobber_zone(struct kunit *test)
 #ifndef CONFIG_KASAN
 static void test_next_pointer(struct kunit *test)
 {
+	if (IS_ENABLED(CONFIG_SLAB_VIRTUAL))
+		kunit_skip(test,
+			"incompatible with freepointer corruption detection in CONFIG_SLAB_VIRTUAL");
+
 	struct kmem_cache *s = test_kmem_cache_create("TestSlub_next_ptr_free",
 							64, SLAB_POISON);
 	u8 *p = kmem_cache_alloc(s, GFP_KERNEL);
